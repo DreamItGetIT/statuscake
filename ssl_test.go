7e680b66313ea3487838cbf88d3ad2b7c500d86c
Alexandre Garand <alexandre.garand@fretlink.com>
2019-06-25 10:25:08 +0200

    fix tests and fixtures

--- ssl_test.go
+++ ssl_test.go
@@ -1,17 +1,19 @@
 package statuscake
 
 import (
-  "testing"
-
+	"testing"
+	//"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"net/url"
 )
+
 func TestSsl_All(t *testing.T) {
-  assert := assert.New(t)
+	assert := assert.New(t)
 	require := require.New(t)
 
 	c := &fakeAPIClient{
-		fixture: "ssls_all_ok.json",
+		fixture: "sslListAllOk.json",
 	}
 	tt := newSsls(c)
 	ssls, err := tt.All()
@@ -20,64 +22,53 @@ func TestSsl_All(t *testing.T) {
 	assert.Equal("/SSL", c.sentRequestPath)
 	assert.Equal("GET", c.sentRequestMethod)
 	assert.Nil(c.sentRequestValues)
-	assert.Len(ssls, 2)
-  mixed := make(map[string]string)
-  mixed["type"]="img"
-  mixed["src"]="http://example.com/image.gif"
-  flags := make(map[string]bool)
-  flags["is_extended"] = false
-  flags["has_pfs"] = true
-  flags["is_broken"] = false
-  flags["is_expired"] = false
-  flags["is_missing"] = false
-  flags["is_revoked"] = false
-  flags["is_mixed"] = false
+	assert.Len(ssls, 3)
+	mixed := make(map[string]string)
+	flags := make(map[string]bool)
+	flags["is_extended"] = false
+	flags["has_pfs"] = true
+	flags["is_broken"] = false
+	flags["is_expired"] = false
+	flags["is_missing"] = false
+	flags["is_revoked"] = false
+	flags["has_mixed"] = false
 	expectedTest := &Ssl{
-    Id: "12345",
-    Paused: false,
-    Domain: "https://google.com",
-    CertScore: "95",
-    CipherScore: "100",
-    CertStatus: "CERT_OK",
-    Cipher: "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
-    ValidFromUtc: "2017-10-10 14:06:00",
-    ValidUntilUtc: "2017-12-29 00:00:00",
-    MixedContent: []map[string]string{mixed},
-    Flags: flags,
-    ContactGroups: []int{12, 13, 14},
-    ContactGroupsC: "12,13,14",
-    AlertAt: "1,7,30",
-    LastReminder: 0,
-    AlertReminder: false,
-    AlertExpiry: false,
-    AlertBroken: false,
-    AlertMixed: false,
-    LastUpdatedUtc: "2017-10-24 09:02:25",
+		Id: "143615",
+		Checkrate: 2073600,
+		Paused: false,
+		Domain: "https://www.fretlink.com",
+		IssuerCn: "Let's Encrypt Authority X3",
+		CertScore: "95",
+		CipherScore: "100",
+		CertStatus: "CERT_OK",
+		Cipher: "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+		ValidFromUtc: "2019-05-28 01:22:00",
+		ValidUntilUtc: "2019-08-26 01:22:00",
+		MixedContent: []map[string]string{},
+		Flags: flags,
+		ContactGroups: []string{},
+		ContactGroupsC: "",
+		AlertAt: "7,18,2019",
+		LastReminder: 2019,
+		AlertReminder: true,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		LastUpdatedUtc: "2019-06-20 10:11:03",
 	}
 	assert.Equal(expectedTest, ssls[0])
 
-	expectedTest = &Ssl{
-	  Id: "12346",
-    Paused: false,
-    Domain: "https://google2.com",
-    CertScore: "95",
-    CipherScore: "100",
-    CertStatus: "CERT_OK",
-    Cipher: "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
-    ValidFromUtc: "2017-10-10 14:06:00",
-    ValidUntilUtc: "2017-12-29 00:00:00",
-    MixedContent: []map[string]string{mixed},
-    Flags: flags,
-    ContactGroups: []int{12, 13, 14},
-    ContactGroupsC: "12,13,14",
-    AlertAt: "1,7,30",
-    LastReminder: 0,
-    AlertReminder: false,
-    AlertExpiry: false,
-    AlertBroken: false,
-    AlertMixed: false,
-    LastUpdatedUtc: "2017-10-24 09:02:25",
-	}
+	expectedTest.Id="143617"
+	expectedTest.LastUpdatedUtc="2019-06-20 10:23:20"
+	assert.Equal(expectedTest, ssls[2])
+
+	expectedTest.Id="143616"
+	expectedTest.LastUpdatedUtc="2019-06-20 10:23:14"
+	mixed["type"]="img"
+	mixed["src"]="http://example.com/image.gif"
+	expectedTest.MixedContent=[]map[string]string{mixed}
+	expectedTest.ContactGroupsC="12,13,34"
+	expectedTest.ContactGroups=[]string{"12","13","34"}
 	assert.Equal(expectedTest, ssls[1])
 }
 
@@ -86,43 +77,93 @@ func TestSsls_Detail_OK(t *testing.T) {
 	require := require.New(t)
 
 	c := &fakeAPIClient{
-		fixture: "ssls_all_ok.json",
+		fixture: "sslListAllOk.json",
 	}
 	tt := newSsls(c)
 
-	ssl, err := tt.Detail("12345")
-  require.Nil(err)
+	ssl, err := tt.Detail("143616")
+	require.Nil(err)
 	assert.Equal("/SSL", c.sentRequestPath)
 	assert.Equal("GET", c.sentRequestMethod)
 	assert.Nil(c.sentRequestValues)
-  assert.Equal(ssl.Domain, "https://google.com")
-  assert.Equal(ssl.Id, "12345")
+	
+	mixed := make(map[string]string)
+	flags := make(map[string]bool)
+
+	mixed["type"]="img"
+	mixed["src"]="http://example.com/image.gif"
+	
+	flags["is_extended"] = false
+	flags["has_pfs"] = true
+	flags["is_broken"] = false
+	flags["is_expired"] = false
+	flags["is_missing"] = false
+	flags["is_revoked"] = false
+	flags["has_mixed"] = false
+	expectedTest := &Ssl{
+		Id: "143616",
+		Checkrate: 2073600,
+		Paused: false,
+		Domain: "https://www.fretlink.com",
+		IssuerCn: "Let's Encrypt Authority X3",
+		CertScore: "95",
+		CipherScore: "100",
+		CertStatus: "CERT_OK",
+		Cipher: "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+		ValidFromUtc: "2019-05-28 01:22:00",
+		ValidUntilUtc: "2019-08-26 01:22:00",
+		MixedContent: []map[string]string{mixed},
+		Flags: flags,
+		ContactGroups: []string{"12","13","34"},
+		ContactGroupsC: "12,13,34",
+		AlertAt: "7,18,2019",
+		LastReminder: 2019,
+		AlertReminder: true,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		LastUpdatedUtc: "2019-06-20 10:23:14",
+	}
+	
+	assert.Equal(expectedTest, ssl)
 }
 
-func TestSsls_UpdatePartialCreate_OK(t *testing.T) {
+func TestSsls_CreatePartial_OK(t *testing.T) {
 	assert := assert.New(t)
 	require := require.New(t)
 
 	c := &fakeAPIClient{
-		fixture: "ssls_create_ok.json",
+		fixture: "sslCreateOk.json",
 	}
 	tt := newSsls(c)
-  partial := &PartialSsl{
-    Domain: "https://example.com",
-  }
-  expectedRes := &PartialSsl {
-    Id: 12345,
-    Domain: "https://example.com",
-    Checkrate: 86400,
-    ContactGroupsC: "1000,2000",
-    AlertReminder: false,
-    AlertExpiry: false,
-    AlertBroken: false,
-    AlertAt: "59,60,61",
-  }
-  res, err := tt.UpdatePartial(partial)
-  require.Nil(err)
-  assert.Equal(expectedRes, res)
+	partial := &PartialSsl{
+		Domain: "https://www.fretlink.com",
+		Checkrate: "2073600",
+		ContactGroupsC: "",
+		AlertReminder: true,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		AlertAt: "7,18,2019",
+	}
+	expectedRes := &PartialSsl {
+		Id: 143616,
+		Domain: "https://www.fretlink.com",
+		Checkrate: "2073600",
+		ContactGroupsC: "",
+		AlertReminder: true,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		AlertAt: "7,18,2019",
+	}
+	res, err := tt.CreatePartial(partial)
+	require.Nil(err)
+	assert.Equal("/SSL/Update", c.sentRequestPath)
+	assert.Equal("PUT", c.sentRequestMethod)
+	assert.Equal(c.sentRequestValues,url.Values(url.Values{"domain":[]string{"https://www.fretlink.com"}, "checkrate":[]string{"2073600"}, "contact_groups":[]string{""}, "alert_at":[]string{"7,18,2019"}, "alert_expiry":[]string{"true"}, "alert_reminder":[]string{"true"}, "alert_broken":[]string{"true"}, "alert_mixed":[]string{"true"}}))
+
+	assert.Equal(expectedRes, res)
 }
 
 func TestSsls_UpdatePartial_OK(t *testing.T) {
@@ -130,44 +171,173 @@ func TestSsls_UpdatePartial_OK(t *testing.T) {
 	require := require.New(t)
 
 	c := &fakeAPIClient{
-		fixture: "ssls_update_ok.json",
+		fixture: "sslUpdateOk.json",
 	}
 	tt := newSsls(c)
-  partial := &PartialSsl{
-    Id: 12345,
-    AlertReminder: true,
-  }
-  expectedRes := &PartialSsl {
-    Id: 12345,
-    AlertReminder: true,
-  }
-  res, err := tt.UpdatePartial(partial)
-  require.Nil(err)
-  assert.Equal(expectedRes, res)
+	partial := &PartialSsl{
+		Id: 143616,
+		Domain: "https://www.fretlink.com",
+		Checkrate: "2073600",
+		ContactGroupsC: "",
+		AlertReminder: false,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		AlertAt: "7,18,2019",
+	}
+	expectedRes := &PartialSsl {
+		Id: 143616,
+		Domain: "https://www.fretlink.com",
+		Checkrate: "2073600",
+		ContactGroupsC: "",
+		AlertReminder: false,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		AlertAt: "7,18,2019",
+	}
+	res, err := tt.UpdatePartial(partial)
+	require.Nil(err)
+	assert.Equal(expectedRes, res)
+	assert.Equal("/SSL/Update", c.sentRequestPath)
+	assert.Equal("PUT", c.sentRequestMethod)
+	assert.Equal(c.sentRequestValues,url.Values(url.Values{"id":[]string{"143616"},"domain":[]string{"https://www.fretlink.com"}, "checkrate":[]string{"2073600"}, "contact_groups":[]string{""}, "alert_at":[]string{"7,18,2019"}, "alert_expiry":[]string{"true"}, "alert_reminder":[]string{"false"}, "alert_broken":[]string{"true"}, "alert_mixed":[]string{"true"}}))
 }
+
 func TestSsl_complete_OK(t *testing.T) {
 	assert := assert.New(t)
 	require := require.New(t)
 
 	c := &fakeAPIClient{
-		fixture: "ssls_all_ok.json",
+		fixture: "sslListAllOk.json",
 	}
 	tt := newSsls(c)
 
-  partial := &PartialSsl {
-    Id: 12345,
-    Domain: "https://example.com",
-    Checkrate: 86400,
-    ContactGroupsC: "1000,2000",
-    AlertReminder: false,
-    AlertExpiry: false,
-    AlertBroken: false,
-    AlertAt: "59,60,61",
-  }
-  full, err := tt.completeSsl(partial)
-  require.Nil(err)
-  assert.Equal(full.Domain, "https://google.com")
-  assert.Equal(full.CipherScore, "100")
-  assert.Equal(full.Checkrate, 86400)
-  assert.Equal(full.ContactGroups, []int{1000, 2000})
+	partial := &PartialSsl {
+		Id: 143616,
+		Domain: "https://www.fretlink.com",
+		Checkrate: "2073600",
+		ContactGroupsC: "12,13,34",
+		AlertReminder: true,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		AlertAt: "7,18,2019",
+	}
+	full, err := tt.completeSsl(partial)
+	require.Nil(err)
+	mixed := make(map[string]string)
+	flags := make(map[string]bool)
+
+	mixed["type"]="img"
+	mixed["src"]="http://example.com/image.gif"
+	
+	flags["is_extended"] = false
+	flags["has_pfs"] = true
+	flags["is_broken"] = false
+	flags["is_expired"] = false
+	flags["is_missing"] = false
+	flags["is_revoked"] = false
+	flags["has_mixed"] = false
+	expectedTest := &Ssl{
+		Id: "143616",
+		Checkrate: 2073600,
+		Paused: false,
+		Domain: "https://www.fretlink.com",
+		IssuerCn: "Let's Encrypt Authority X3",
+		CertScore: "95",
+		CipherScore: "100",
+		CertStatus: "CERT_OK",
+		Cipher: "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+		ValidFromUtc: "2019-05-28 01:22:00",
+		ValidUntilUtc: "2019-08-26 01:22:00",
+		MixedContent: []map[string]string{mixed},
+		Flags: flags,
+		ContactGroups: []string{"12","13","34"},
+		ContactGroupsC: "12,13,34",
+		AlertAt: "7,18,2019",
+		LastReminder: 2019,
+		AlertReminder: true,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		LastUpdatedUtc: "2019-06-20 10:23:14",
+	}
+	
+	assert.Equal(expectedTest, full)
+	
+}
+
+func TestSsl_partial_OK(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+	
+	mixed := make(map[string]string)
+	flags := make(map[string]bool)
+
+	mixed["type"]="img"
+	mixed["src"]="http://example.com/image.gif"
+	
+	flags["is_extended"] = false
+	flags["has_pfs"] = true
+	flags["is_broken"] = false
+	flags["is_expired"] = false
+	flags["is_missing"] = false
+	flags["is_revoked"] = false
+	flags["has_mixed"] = false
+	full := &Ssl{
+		Id: "143616",
+		Checkrate: 2073600,
+		Paused: false,
+		Domain: "https://www.fretlink.com",
+		IssuerCn: "Let's Encrypt Authority X3",
+		CertScore: "95",
+		CipherScore: "100",
+		CertStatus: "CERT_OK",
+		Cipher: "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+		ValidFromUtc: "2019-05-28 01:22:00",
+		ValidUntilUtc: "2019-08-26 01:22:00",
+		MixedContent: []map[string]string{mixed},
+		Flags: flags,
+		ContactGroups: []string{"12","13","34"},
+		ContactGroupsC: "12,13,34",
+		AlertAt: "7,18,2019",
+		LastReminder: 2019,
+		AlertReminder: true,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		LastUpdatedUtc: "2019-06-20 10:23:14",
+	}
+	expectedTest:=&PartialSsl {
+		Id: 143616,
+		Domain: "https://www.fretlink.com",
+		Checkrate: "2073600",
+		ContactGroupsC: "12,13,34",
+		AlertReminder: true,
+		AlertExpiry: true,
+		AlertBroken: true,
+		AlertMixed: true,
+		AlertAt: "7,18,2019",
+	}
+	partial,err:=Partial(full)
+	require.Nil(err)
+	assert.Equal(expectedTest, partial)
+	
+}
+
+func TestSsls_Delete_OK(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	c := &fakeAPIClient{
+		fixture: "sslDeleteOk.json",
+	}
+	tt := newSsls(c)
+
+	err := tt.Delete("143616")
+	require.Nil(err)
+	assert.Equal("/SSL/Update", c.sentRequestPath)
+	assert.Equal("DELETE", c.sentRequestMethod)
+	assert.Equal(c.sentRequestValues,url.Values(url.Values{"id":[]string{"143616"},},))
 }
